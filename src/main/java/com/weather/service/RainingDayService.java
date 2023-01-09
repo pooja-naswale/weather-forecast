40070675c03f3f057bfce01a63145a873195c9ab
Pandurang Chate <pandurang_chate@persistent.com>
2023-01-09 21:07:21 +0530

    The RainingDayService class is changed by Pandurang with addition of SYSOUT statement...

--- src/main/java/com/weather/service/RainingDayService.java
+++ src/main/java/com/weather/service/RainingDayService.java
@@ -7,6 +7,7 @@ public class RainingDayService implements WeatherService {
 
     @Override
     public String forecast() {
+        System.out.println("Raining Day Service Feature modified by Pandurang...")
         return "Today is raining day!";
     }
 
