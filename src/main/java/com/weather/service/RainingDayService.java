66fcd23cb5ab2a1315d334aefc8e26d7a6ed73ec
Pandurang Chate <pandurang_chate@persistent.com>
2023-01-10 13:14:45 +0530

    Updated RainyDay Service by Pandurang

--- src/main/java/com/weather/service/RainingDayService.java
+++ src/main/java/com/weather/service/RainingDayService.java
@@ -7,6 +7,7 @@ public class RainingDayService implements WeatherService {
 
     @Override
     public String forecast() {
+        System.out.println("RainingDay Feature added by Pandurang");
         return "Today is raining day!";
     }
 
